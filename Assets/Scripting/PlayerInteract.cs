02fbd9cbd08e64bcad74153311388544d1d2e700
RileyWren <zainab.42847@gmail.com>
2026-04-27 12:35:54 +0600

    Creating player intarection

--- Assets/Scripting/PlayerInteract.cs
+++ Assets/Scripting/PlayerInteract.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
     private Camera cam;
+    [SerializeField]
     public float distance = 3f;
+    [SerializeField]
+    private LayerMask mask;
 
     void Start()
     {
@@ -16,5 +21,11 @@ public class PlayerInteract : MonoBehaviour
     {
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * distance);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, distance, mask))
+        {
+            
+        }
+
     }
 }
