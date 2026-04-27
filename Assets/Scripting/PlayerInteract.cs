87c0fbb11fa245589438af455e9e9189f5519a23
RileyWren <zainab.42847@gmail.com>
2026-04-27 08:31:51 +0600

    Creating PLayer Inrarection system

---
+++ Assets/Scripting/PlayerInteract.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private Camera cam;
+    public float distance = 3f;
+
+    void Start()
+    {
+        cam = GetComponent<PlayerLook>().cam;
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance);
+    }
+}
